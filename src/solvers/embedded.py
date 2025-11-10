49b15de95dd122880d377671edff0fd22359fe0f
Leo <l.kellerer1@gmx.de>
2025-11-10 14:37:57 +0100

    bugs fixed

--- src/solvers/embedded.py
+++ src/solvers/embedded.py
@@ -1,26 +1,32 @@
 from typing import Any, Callable
 import numpy as np
 from numpy.typing import NDArray
+from solvers.helpers import norm_hairer, controller_I, controller_PI
+
 
 def DP45(
     f: Callable[[float, NDArray[np.floating]], NDArray[np.floating]],
     x0: NDArray[np.floating],
     t_max: float,
-    h: float,
     t0: float = 0.0,
-    eps: float = 10**-5,
-    s_clip: tuple[float, float] = (0.2, 5.),
-    tol_safety: float = 0.9,
-    h_limits: tuple[float, float] = (0, np.inf)
+    atol: float | NDArray[np.floating] = 10**-5,
+    rtol: float | NDArray[np.floating] = 10**-3,
+    s_clip: tuple[float, float] = (0.2, 5.0),
+    step_safety: float = 0.9,
+    h_limits: tuple[float, float] = (0, np.inf),
+    norm: Callable[[NDArray[np.floating]], float] = norm_hairer,
 ) -> tuple[NDArray[np.floating], NDArray[np.floating], dict[str, Any]]:
     """Dormand Prince 5(4) Method, MATLAB ode45 solver"""
-    h = tol_safety * eps ** (1 / 5) / 4
+    # h = tol_safety * eps ** (1 / 5) / 4
+    h = (tol / (1 / (t_max - t0) ** (1 / 5) + norm(f(t0, x0)) ** (1 / 5))) ** (
+        1 / 5
+    )  # TODO: check exponents
 
     info: dict[str, Any] = dict(
-      n_feval=0,
-      n_jaceval=0,
-      n_lu=0,
-      n_restarts=0,
+        n_feval=0,
+        n_jaceval=0,
+        n_lu=0,
+        n_restarts=0,
     )
 
     t = [t0]
@@ -29,7 +35,7 @@ def DP45(
     x_crit = []
 
     k = 0
-    k1 = f(t[0], x[0]) # FSAL property
+    k1 = f(t[0], x[0])  # FSAL property
     while t[k] < t_max:  # iterate until t_max is reached
         if t[k] + h > t_max:  # shorten h if we would go further than necessary
             h = t_max - t[k]
@@ -75,69 +81,79 @@ def DP45(
             t_pred, x_pred
         )  # reusing k2 instead of new variable k7, as it is not used again
 
-        err = h * np.linalg.norm(
+        err = h * norm(
             71 / 57600 * k1
             - 71 / 16695 * k3
             + 71 / 1920 * k4
             - 17253 / 339200 * k5
             + 22 / 525 * k6
             - 1 / 40 * k2,
-            ord=np.inf,
         )  # h*np.linalg.norm(71/57600*k1-71/16695*k3+71/1920*k4-17253/339200*k5+22/525*k6-1/40*k2, ord=np.inf) #local error:||x_pred-X_pred7||
         if err == 0:  # set s to max tp prevent divide by zero error
             s = s_clip[1]
         else:
             s = np.clip(
-                tol_safety * (eps / err) ** (1 / 5), s_clip[0], s_clip[1]
+                step_safety * (tol / err) ** (1 / 5), s_clip[0], s_clip[1]
             )  # s gets clipped to prevent to extreme changes of h, also err might become zero
 
-        if (err < eps or h <= h_limits[0]): #accept result if tolerance is met, or we cant decrease h anymore
-            k1 = k2
+        if (
+            err_ratio > 1 or h <= h_limits[0]
+        ):  # accept result if tolerance is met, or we cant decrease h anymore
+            k1 = k4
+            err_ratio_last = err_ratio
             k += 1
             x.append(x_pred)
             t.append(t_pred)
         else:
-            if s > 1:
-                print("h gets increased to decrease error? This shouldnt happen!")
             t_crit.append(t_pred)
             x_crit.append(x_pred)
 
-        h = h * s
-        h = np.clip(h, h_limits[0], h_limits[1])
-    info['n_feval'] = (k + len(t_crit))*6
-    info['n_restarts'] = len(t_crit)
-    info['restarts'] = (t_crit, x_crit)
-    return t, x, info
+        h = np.clip(h * s, h_limits[0], h_limits[1])
+    info["n_feval"] = (k + len(t_crit)) * 6
+    info["n_restarts"] = len(t_crit)
+    info["restarts"] = (t_crit, x_crit)
+    return np.array(t), np.array(x), info
 
 
 def BS23(
     f: Callable[[float, NDArray[np.floating]], NDArray[np.floating]],
     x0: NDArray[np.floating],
     t_max: float,
-    h: float,
     t0: float = 0.0,
-    eps: float = 10**-5,
-    s_clip: tuple[float, float] = (0.2, 5.),
-    tol_safety: float = 0.9
-    h_limits: tuple[float, float] = (0, np.inf)
-    ) -> tuple[NDArray[np.floating], NDArray[np.floating], dict[str, Any]]:
+    atol: float | NDArray[np.floating] = 10**-5,
+    rtol: float | NDArray[np.floating] = 10**-3,
+    step_controller: Callable[
+        [float, float], float
+    ] = lambda err_ratio, err_ratio_last: controller_PI(
+        err_ratio, err_ratio_last, alpha, beta
+    ),
+    s_clip: tuple[float, float] = (0.2, 5.0),
+    step_safety: float = 0.9,
+    h_limits: tuple[float, float] = (0, np.inf),
+    norm: Callable[[NDArray[np.floating]], float] = norm_hairer,
+) -> tuple[NDArray[np.floating], NDArray[np.floating], dict[str, Any]]:
     """Bogacki–Shampine Method, MATLAB ode23 solver"""
-    h = tol_safety * eps ** (1 / 3) / 4  # t_max-t0
+    # h = tol_safety * eps ** (1 / 3) / 4  # t_max-t0
+    tol = atol + rtol * np.abs(x0)
+    h = (tol / (1 / (t_max - t0) ** (1 / 5) + norm(f(t0, x0)) ** (1 / 5))) ** (
+        1 / 5
+    )  # TODO: check exponents
 
     info: dict[str, Any] = dict(
-      n_feval=0,
-      n_jaceval=0,
-      n_lu=0,
-      n_restarts=0,
+        n_feval=0,
+        n_jaceval=0,
+        n_lu=0,
+        n_restarts=0,
     )
 
     t = [t0]
     x = [x0]
     t_crit = []
     x_crit = []
+    err_ratio_last = 
 
     k = 0
-    k1 = f(t[0], x[0]) # FSAL
+    k1 = f(t[0], x[0])  # FSAL
     while t[k] < t_max:  # iterate until t_max is reached
         if t[k] + h > t_max:  # shorten h if we would go further than necessary
             h = t_max - t[k]
@@ -149,32 +165,36 @@ def BS23(
         x_pred = x[k] + h * (2 * k1 + 3 * k2 + 4 * k3) / 9
         k4 = f(t_pred, x_pred)
 
-        err = h * np.linalg.norm(
-            -5 / 72 * k1 + 1 / 12 * k2 + 1 / 9 * k3 - 1 / 8 * k4, ord=np.inf
-        )  # h*np.linalg.norm(71/57600*k1-71/16695*k3+71/1920*k4-17253/339200*k5+22/525*k6-1/40*k2, ord=np.inf) #local error:||x_pred-X_pred7||
-        if err == 0:  # set s to max tp prevent divide by zero error
-            s = s_clip[1]
-        else:
-            s = np.clip(
-                tol_safety * (eps / err) ** (1 / 3), s_clip[0], s_clip[1]
-            )  # s gets clipped to prevent to extreme changes of h, also err might become zero
+        err = -5 / 72 * k1 + 1 / 12 * k2 + 1 / 9 * k3 - 1 / 8 * k4
+        # h*np.linalg.norm(71/57600*k1-71/16695*k3+71/1920*k4-17253/339200*k5+22/525*k6-1/40*k2, ord=np.inf) #local error:||x_pred-X_pred7||
+        tol = atol + rtol * np.maximum(np.abs(x0), np.abs(err))
+        err_ratio = norm(h * err / tol)  # TODO: h required?
+        # TODO: divide by zero errors
+
+        # TODO: init err_ratio
+        # TODO: beta
+        # TODO: steady limits?
+        # TODO: make class
+        s = np.clip(
+            step_safety * step_controller(err_ratio, err_ratio_last),
+            s_clip[0],
+            s_clip[1],
+        )  # s gets clipped to prevent to extreme changes of h, also err might become zero
 
         if (
-            err < eps
-        or h <= h_limits[0]): #accept result if tolerance is met, or we cant decrease h anymore
+            err_ratio > 1 or h <= h_limits[0]
+        ):  # accept result if tolerance is met, or we cant decrease h anymore
             k1 = k4
+            err_ratio_last = err_ratio
             k += 1
             x.append(x_pred)
             t.append(t_pred)
         else:
-            if s > 1:
-                print("h gets increased to decrease error? This shouldnt happen!")
             t_crit.append(t_pred)
             x_crit.append(x_pred)
 
-        h = h * s
-        h=np.clip(h, h_limits[0], h_limits[1])
-    info['n_feval'] = (len(t)-1 + len(t_crit))*3
-    info['n_restarts'] = len(t_crit)
-    info['restarts'] = (t_crit, x_crit)
-    return t, x, info
+        h = np.clip(h * s, h_limits[0], h_limits[1])
+    info["n_feval"] = (len(t) - 1 + len(t_crit)) * 3
+    info["n_restarts"] = len(t_crit)
+    info["restarts"] = (t_crit, x_crit)
+    return np.array(t), np.array(x), info
--- src/solvers/explicit.py
+++ src/solvers/explicit.py
@@ -41,27 +41,22 @@ def Midpoint(
     t0: float = 0.0,
 ) -> tuple[NDArray[np.floating], NDArray[np.floating], dict[str, Any]]:
     """Explicit Midpoint method for numerical solving of ODEs of the form x_dot=f(t,x)"""
-    steps = np.ceil((t_max - t0) / h) + 1
-            info: dict[str, Any] = dict(
-            n_feval=0,
-            n_jaceval=0,
-            n_lu=0,
-            n_restarts=0,
-        )
+    steps = np.ceil((t_max - t0) / h).astype(int)
+    if steps * h / (t_max - t0) - 1.0 > 1e-4:
+        print("final step not hitting t_max exactly")
 
     info: dict[str, Any] = dict(
-            n_feval=0,
-            n_jaceval=0,
-            n_lu=0,
-            n_restarts=0,
-        )
+        n_feval=0,
+        n_jaceval=0,
+        n_lu=0,
+        n_restarts=0,
+    )
 
-
-    t = np.linspace(t0, t_max, steps)
-    x = np.zeros((x0.shape[0], steps))
-    t[:, 0] = t0
-    x[:, :1] = x0
-    for i in range(steps - 1):
+    t = np.linspace(t0, t_max, steps + 1)
+    x = np.zeros((x0.shape[0], steps + 1))
+    t[0] = t0
+    x[:, 0] = x0
+    for i in range(steps):
         # t[:,i+1:i+2]=t[:, i:i+1]+h
         x[:, i + 1] = x[:, i] + h * f(
             t[i] + 0.5 * h,
@@ -80,28 +75,31 @@ def AB2(
     t0: float = 0.0,
 ) -> tuple[NDArray[np.floating], NDArray[np.floating], dict[str, Any]]:
     """Adams Bashforth of order 2, started by Midpoint method"""
-    steps = np.ceil((t_max - t0) / h) + 1
+    steps = np.ceil((t_max - t0) / h).astype(int)
+    if steps * h / (t_max - t0) - 1.0 > 1e-4:
+        print("final step not hitting t_max exactly")
 
     info: dict[str, Any] = dict(
-            n_feval=0,
-            n_jaceval=0,
-            n_lu=0,
-            n_restarts=0,
-        )
+        n_feval=0,
+        n_jaceval=0,
+        n_lu=0,
+        n_restarts=0,
+    )
 
     t = np.linspace(t0, steps * h, steps + 1, dtype=x0.dtype)
     x = np.zeros((x0.shape[0], steps + 1), dtype=x0.dtype)
     _, x[:, :2], inf_starter = Midpoint(f, x0, t0 + h, h, t0)
     info = inf_starter
 
-    for i in range(1, steps - 1):
-        # t[:,i+1:i+2]=t[:, i:i+1]+h
-        x[:, i + 1] = x[:, i] + h / 2 * (
-            3 * f(t[i], x[:, i])
-            - f(t[i-1], x[:, i-1])
-        )
+    f_i = f(
+        t[0], x[:, 0]
+    )  # TODO: this has already been evaluated in the starting method
+    for i in range(1, steps):
+        f_ii = f_i
+        f_i = f(t[i], x[:, i])
+        x[:, i + 1] = x[:, i] + h / 2 * (3 * f_i - f_ii)
 
-    info['n_feval'] += 2 * (steps-1)
+    info["n_feval"] += steps - 1
     return t, x, info
 
 
@@ -113,7 +111,9 @@ def AB3(
     t0: float = 0.0,
 ) -> tuple[NDArray[np.floating], NDArray[np.floating], dict[str, Any]]:
     """Adams Bashforth of order 3, first values calculated with Midpoint and AB2"""
-    steps = np.ceil((t_max - t0) / h) + 1
+    steps = np.ceil((t_max - t0) / h).astype(int)
+    if steps * h / (t_max - t0) - 1.0 > 1e-4:
+        print("final step not hitting t_max exactly")
 
     info: dict[str, Any] = dict(
         n_feval=0,
@@ -127,14 +127,18 @@ def AB3(
     _, x[:, :3], inf_starter = AB2(f, x0, t0 + 2 * h, h, t0)
     info = inf_starter
 
-    for i in range(2, steps - 1):
+    f_i = f(
+        t[0], x[:, 0]
+    )  # TODO: this has already been evaluated in the starting method
+    f_ii = f(t[1], x[:, 1])
+
+    for i in range(2, steps):
         # t[:,i+1:i+2]=t[:, i:i+1]+h
-        x[:, i + 1] = x[:, i] + h / 12 * (
-            23 * f(t[i], x[:, i ])
-            - 16 * f(t[i - 1], x[:, i - 1])
-            + 5 * f(t[i - 2 ], x[:, i - 2])
-        )
-    info['n_feval'] += 3 * (steps-2)
+        f_iii = f_ii
+        f_ii = f_i
+        f_i = f(t[i], x[:, i])
+        x[:, i + 1] = x[:, i] + h / 12 * (23 * f_i - 16 * f_ii + 5 * f_iii)
+    info["n_feval"] += steps - 2
     return t, x, info
 
 
@@ -146,9 +150,10 @@ def PECE(
     n_rep: int = 1,
     t0: float = 0.0,
 ) -> tuple[NDArray[np.floating], NDArray[np.floating], dict[str, Any]]:
-
     """PECE Method using AB3, AM4, starting with Midpoint and AB2"""
-    steps = np.ceil((t_max - t0) / h) + 1
+    steps = np.ceil((t_max - t0) / h).astype(int)
+    if steps * h / (t_max - t0) - 1.0 > 1e-4:
+        print("final step not hitting t_max exactly")
 
     info: dict[str, Any] = dict(
         n_feval=0,
@@ -163,26 +168,31 @@ def PECE(
     _, x[:, :3], inf_starter = AB2(f, x0, t0 + 2 * h, h, t0)
     info = inf_starter
 
-    for i in range(2, steps - 1):
+    f_i = f(
+        t[0], x[:, 0]
+    )  # TODO: this has already been evaluated in the starting method
+    f_ii = f(t[1], x[:, 1])
+    for i in range(2, steps):
         # t[:,i+1:i+2]=t[:, i:i+1]+h
-        f_i  = f(t[i], x[:, i])
-        f_ii = f(t[i - 1], x[:, i - 1])
-        f_iii = f(t[i - 2], x[:, i - 2 ])
+        f_iii = f_ii
+        f_ii = f_i
+        f_i = f(t[i], x[:, i])
 
-        x[:, i + 1] = x[:, i] + h / 12 * (23 * f_i - 16 * f_ii + 5 * f_iii)  # AB3 predict/evaluate
-        k = x[:, i] + h / 24 * (
-            19 * f_i
-            - 5 * f_ii
-            + f_iii
-        )
+        x[:, i + 1] = x[:, i] + h / 12 * (
+            23 * f_i - 16 * f_ii + 5 * f_iii
+        )  # AB3 predict/evaluate
+        k = x[:, i] + h / 24 * (19 * f_i - 5 * f_ii + f_iii)
         for _ in range(n_rep):
-            x[:, i + 1] = k + 9 * h / 24 * f(t[i + 1], x[:, i + 1])  # AM4 correct/evaluate loop
-    info['n_feval'] += (3+n_rep) * (steps-2)
+            x[:, i + 1] = k + 9 * h / 24 * f(
+                t[i + 1], x[:, i + 1]
+            )  # AM4 correct/evaluate loop
+    info["n_feval"] += (1 + n_rep) * (steps - 2)
     return t, x, info
 
 
 #######################   UNTESTED   ######################
-def PECE(    f: Callable[[float, NDArray[np.floating]], NDArray[np.floating]],
+def PECE_(
+    f: Callable[[float, NDArray[np.floating]], NDArray[np.floating]],
     x0: NDArray[np.floating],
     t_max: float,
     h: float,
@@ -190,7 +200,9 @@ def PECE(    f: Callable[[float, NDArray[np.floating]], NDArray[np.floating]],
     t0: float = 0.0,
 ) -> tuple[NDArray[np.floating], NDArray[np.floating], dict[str, Any]]:
     """PECE Method using AB3, AM4, starting with Midpoint and AB2, iterates until convergence with tolerance tol is met"""
-    steps = np.ceil((t_max - t0) / h) + 1
+    steps = np.ceil((t_max - t0) / h).astype(int)
+    if steps * h / (t_max - t0) - 1.0 > 1e-4:
+        print("final step not hitting t_max exactly")
 
     info: dict[str, Any] = dict(
         n_feval=0,
@@ -204,18 +216,16 @@ def PECE(    f: Callable[[float, NDArray[np.floating]], NDArray[np.floating]],
     _, x[:, :3], inf_starter = AB2(f, x0, t0 + 2 * h, h, t0)
     info = inf_starter
 
-    for i in range(2, steps - 1):
+    for i in range(2, steps):
         # t[:,i+1:i+2]=t[:, i:i+1]+h
-        f_i  = f(t[i], x[:, i])
-        f_ii = f(t[i - 1], x[:, i - 1])
-        f_iii = f(t[i - 2], x[:, i - 2 ])
+        f_iii = f_ii
+        f_ii = f_i
+        f_i = f(t[i], x[:, i])
 
-        x[:, i + 1] = x[:, i] + h / 12 * (23 * f_i - 16 * f_ii + 5 * f_iii)  # AB3 predict/evaluate
-        k = x[:, i] + h / 24 * (
-            19 * f_i
-            - 5 * f_ii
-            + f_iii
-        )
+        x[:, i + 1] = x[:, i] + h / 12 * (
+            23 * f_i - 16 * f_ii + 5 * f_iii
+        )  # AB3 predict/evaluate
+        k = x[:, i] + h / 24 * (19 * f_i - 5 * f_ii + f_iii)
 
         last = np.inf
         while np.linalg.norm(x[:, i + 1] - last, ord=np.inf) > tol:
@@ -223,9 +233,9 @@ def PECE(    f: Callable[[float, NDArray[np.floating]], NDArray[np.floating]],
             x[:, i + 1] = k + 9 * h / 24 * f(
                 t[i + 1], x[:, i + 1]
             )  # AM4 correct/evaluate loop
-            info['n_feval'] += 1
+            info["n_feval"] += 1
 
-    info['n_feval'] += 3 * (steps-2)
+    info["n_feval"] += steps - 2
     return t, x, info
 
 
@@ -259,7 +269,7 @@ def RK4(
         k4 = f(t[i] + h, x[:, i] + h * k3)
         x[:, i + 1] = x[:, i] + h / 6 * (k1 + 2 * k2 + 2 * k3 + k4)
 
-    info["n_feval"] = 4*steps
+    info["n_feval"] = 4 * steps
     return t, x, info
 
 
@@ -288,11 +298,12 @@ def SSPRK3(
     x[:, 0] = x0
     for i in range(steps):
         x1 = f(t[i], x[:, i])
-        x2 = 3./4.*x[:, i] + 1./4.*f(t[i] + h, x1)
-        x[:, i + 1] = 1./3.*x[:, i] + 2./3.*f(t[i] + h/2., x2)
+        x2 = 3.0 / 4.0 * x[:, i] + 1.0 / 4.0 * f(t[i] + h, x1)
+        x[:, i + 1] = 1.0 / 3.0 * x[:, i] + 2.0 / 3.0 * f(t[i] + h / 2.0, x2)
 
-    info["n_feval"] = 3*steps
+    info["n_feval"] = 3 * steps
     return t, x, info
+
 
 def SSPRK34(
     f: Callable[[float, NDArray[np.floating]], NDArray[np.floating]],
@@ -318,12 +329,13 @@ def SSPRK34(
     t[0] = t0
     x[:, 0] = x0
     for i in range(steps):
-        x1 = 1./2.*x[:, i + 1] + 1./2.*f(t[i], x[:, i])
-        x2 = 1./2.*x1 + 1./2.*f(t[i] + h/2., x1)
-        x3 = 2./3.*x[:, i + 1] + 1./6.*x2 + 1./6.*f(t[i] + h, x2)
-        x[:, i + 1] = 1./2.*x3 + 1./2.*f(t[i] + h/2., x3)
+        x1 = 1.0 / 2.0 * x[:, i + 1] + 1.0 / 2.0 * f(t[i], x[:, i])
+        x2 = 1.0 / 2.0 * x1 + 1.0 / 2.0 * f(t[i] + h / 2.0, x1)
+        x3 = 2.0 / 3.0 * x[:, i + 1] + 1.0 / 6.0 * x2 + 1.0 / 6.0 * f(t[i] + h, x2)
+        x[:, i + 1] = 1.0 / 2.0 * x3 + 1.0 / 2.0 * f(t[i] + h / 2.0, x3)
 
-    info["n_feval"] = 4*steps
+    info["n_feval"] = 4 * steps
     return t, x, info
+
 
 # TODO: add Radau, TSRK, RK853
