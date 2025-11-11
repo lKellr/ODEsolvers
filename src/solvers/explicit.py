afc2fbc522c65089b0ddb4820991ed9fdd2ba049
Leo <l.kellerer1@gmx.de>
2025-11-11 23:34:22 +0100

    fixed steps bug

--- src/solvers/explicit.py
+++ src/solvers/explicit.py
@@ -23,12 +23,11 @@ def Euler(
     )
 
     t = np.linspace(t0, steps * h, steps + 1, dtype=x0.dtype)
-    x = np.zeros((x0.shape[0], steps + 1), dtype=x0.dtype)
+    x = np.zeros((steps + 1, x0.shape[0]), dtype=x0.dtype)
     t[0] = t0
-    x[:, 0] = x0
+    x[0] = x0
     for i in range(steps):
-        # t[:,i+1:i+2]=t[:, i:i+1]+h
-        x[:, i + 1 : i + 2] = x[:, i : i + 1] + h * f(t[i], x[:, i : i + 1])
+        x[i + 1] = x[i] + h * f(t[i], x[i])
     info["n_feval"] = steps
     return t, x, info
 
@@ -53,14 +52,13 @@ def Midpoint(
     )
 
     t = np.linspace(t0, t_max, steps + 1)
-    x = np.zeros((x0.shape[0], steps + 1))
+    x = np.zeros((steps + 1, x0.shape[0]), dtype=x0.dtype)
     t[0] = t0
-    x[:, 0] = x0
+    x[0] = x0
     for i in range(steps):
-        # t[:,i+1:i+2]=t[:, i:i+1]+h
-        x[:, i + 1] = x[:, i] + h * f(
+        x[i + 1] = x[i] + h * f(
             t[i] + 0.5 * h,
-            x[:, i] + 0.5 * h * f(t[i], x[:, i]),
+            x[i] + 0.5 * h * f(t[i], x[i]),
         )
 
     info["n_feval"] = 2 * steps
@@ -87,12 +85,12 @@ def Heun(
     )
 
     t = np.linspace(t0, t_max, steps + 1)
-    x = np.zeros((x0.shape[0], steps + 1))
+    x = np.zeros((steps + 1, x0.shape[0]), dtype=x0.dtype)
     t[0] = t0
-    x[:, 0] = x0
+    x[0] = x0
     for i in range(steps):
-        x1 = x[:, i] + h * f(t[i], x[:, i])
-        x[:, i + 1] = 0.5 * x[:, i] + 0.5 * (x1 + h * f(t[i] + h, x1))
+        x1 = x[i] + h * f(t[i], x[i])
+        x[i + 1] = 0.5 * x[i] + 0.5 * (x1 + h * f(t[i] + h, x1))
 
     info["n_feval"] = 2 * steps
     return t, x, info
@@ -118,17 +116,15 @@ def AB2(
     )
 
     t = np.linspace(t0, steps * h, steps + 1, dtype=x0.dtype)
-    x = np.zeros((x0.shape[0], steps + 1), dtype=x0.dtype)
-    _, x[:, :2], inf_starter = Midpoint(f, x0, t0 + h, h, t0)
+    x = np.zeros((steps + 1, x0.shape[0]), dtype=x0.dtype)
+    _, x[:2], inf_starter = Midpoint(f, x0, t0 + h, h, t0)
     info = inf_starter
 
-    f_i = f(
-        t[0], x[:, 0]
-    )  # TODO: this has already been evaluated in the starting method
+    f_i = f(t[0], x[0])  # TODO: this has already been evaluated in the starting method
     for i in range(1, steps):
         f_ii = f_i
-        f_i = f(t[i], x[:, i])
-        x[:, i + 1] = x[:, i] + h / 2 * (3 * f_i - f_ii)
+        f_i = f(t[i], x[i])
+        x[i + 1] = x[i] + h / 2 * (3 * f_i - f_ii)
 
     info["n_feval"] += steps - 1
     return t, x, info
@@ -154,21 +150,19 @@ def AB3(
     )
 
     t = np.linspace(t0, steps * h, steps + 1, dtype=x0.dtype)
-    x = np.zeros((x0.shape[0], steps + 1), dtype=x0.dtype)
-    _, x[:, :3], inf_starter = AB2(f, x0, t0 + 2 * h, h, t0)
+    x = np.zeros((steps + 1, x0.shape[0]), dtype=x0.dtype)
+    _, x[:3], inf_starter = AB2(f, x0, t0 + 2 * h, h, t0)
     info = inf_starter
 
-    f_i = f(
-        t[0], x[:, 0]
-    )  # TODO: this has already been evaluated in the starting method
-    f_ii = f(t[1], x[:, 1])
+    f_i = f(t[0], x[0])  # TODO: this has already been evaluated in the starting method
+    f_ii = f(t[1], x[1])
 
     for i in range(2, steps):
         # t[:,i+1:i+2]=t[:, i:i+1]+h
         f_iii = f_ii
         f_ii = f_i
-        f_i = f(t[i], x[:, i])
-        x[:, i + 1] = x[:, i] + h / 12 * (23 * f_i - 16 * f_ii + 5 * f_iii)
+        f_i = f(t[i], x[i])
+        x[i + 1] = x[i] + h / 12 * (23 * f_i - 16 * f_ii + 5 * f_iii)
     info["n_feval"] += steps - 2
     return t, x, info
 
@@ -194,28 +188,26 @@ def PECE(
     )
 
     t = np.linspace(t0, steps * h, steps + 1, dtype=x0.dtype)
-    x = np.zeros((x0.shape[0], steps + 1), dtype=x0.dtype)
+    x = np.zeros((steps + 1, x0.shape[0]), dtype=x0.dtype)
     # t[:,:3], x[:,:3]=AB2(f, x0, t0+2*h, h, t0)
-    _, x[:, :3], inf_starter = AB2(f, x0, t0 + 2 * h, h, t0)
+    _, x[:3], inf_starter = AB2(f, x0, t0 + 2 * h, h, t0)
     info = inf_starter
 
-    f_i = f(
-        t[0], x[:, 0]
-    )  # TODO: this has already been evaluated in the starting method
-    f_ii = f(t[1], x[:, 1])
+    f_i = f(t[0], x[0])  # TODO: this has already been evaluated in the starting method
+    f_ii = f(t[1], x[1])
     for i in range(2, steps):
         # t[:,i+1:i+2]=t[:, i:i+1]+h
         f_iii = f_ii
         f_ii = f_i
-        f_i = f(t[i], x[:, i])
+        f_i = f(t[i], x[i])
 
-        x[:, i + 1] = x[:, i] + h / 12 * (
+        x[i + 1] = x[i] + h / 12 * (
             23 * f_i - 16 * f_ii + 5 * f_iii
         )  # AB3 predict/evaluate
-        k = x[:, i] + h / 24 * (19 * f_i - 5 * f_ii + f_iii)
+        k = x[i] + h / 24 * (19 * f_i - 5 * f_ii + f_iii)
         for _ in range(n_rep):
-            x[:, i + 1] = k + 9 * h / 24 * f(
-                t[i + 1], x[:, i + 1]
+            x[i + 1] = k + 9 * h / 24 * f(
+                t[i + 1], x[i + 1]
             )  # AM4 correct/evaluate loop
     info["n_feval"] += (1 + n_rep) * (steps - 2)
     return t, x, info
@@ -243,26 +235,28 @@ def PECE_(
     )
 
     t = np.linspace(t0, steps * h, steps + 1, dtype=x0.dtype)
-    x = np.zeros((x0.shape[0], steps + 1), dtype=x0.dtype)
-    _, x[:, :3], inf_starter = AB2(f, x0, t0 + 2 * h, h, t0)
+    x = np.zeros((steps + 1, x0.shape[0]), dtype=x0.dtype)
+    _, x[:3], inf_starter = AB2(f, x0, t0 + 2 * h, h, t0)
     info = inf_starter
 
+    f_i = f(t[0], x[0])  # TODO: this has already been evaluated in the starting method
+    f_ii = f(t[1], x[1])
     for i in range(2, steps):
         # t[:,i+1:i+2]=t[:, i:i+1]+h
         f_iii = f_ii
         f_ii = f_i
-        f_i = f(t[i], x[:, i])
+        f_i = f(t[i], x[i])
 
-        x[:, i + 1] = x[:, i] + h / 12 * (
+        x[i + 1] = x[i] + h / 12 * (
             23 * f_i - 16 * f_ii + 5 * f_iii
         )  # AB3 predict/evaluate
-        k = x[:, i] + h / 24 * (19 * f_i - 5 * f_ii + f_iii)
+        k = x[i] + h / 24 * (19 * f_i - 5 * f_ii + f_iii)
 
         last = np.inf
-        while np.linalg.norm(x[:, i + 1] - last, ord=np.inf) > tol:
-            last = x[:, i + 1]
-            x[:, i + 1] = k + 9 * h / 24 * f(
-                t[i + 1], x[:, i + 1]
+        while np.linalg.norm(x[i + 1] - last, ord=np.inf) > tol:
+            last = x[i + 1]
+            x[i + 1] = k + 9 * h / 24 * f(
+                t[i + 1], x[i + 1]
             )  # AM4 correct/evaluate loop
             info["n_feval"] += 1
 
@@ -290,15 +284,15 @@ def RK4(
     )
 
     t = np.linspace(t0, steps * h, steps + 1, dtype=x0.dtype)
-    x = np.zeros((x0.shape[0], steps + 1), dtype=x0.dtype)
+    x = np.zeros((steps + 1, x0.shape[0]), dtype=x0.dtype)
     t[0] = t0
-    x[:, 0] = x0
+    x[0] = x0
     for i in range(steps):
-        k1 = f(t[i], x[:, i])
-        k2 = f(t[i] + 0.5 * h, x[:, i] + 0.5 * h * k1)
-        k3 = f(t[i] + 0.5 * h, x[:, i] + 0.5 * h * k2)
-        k4 = f(t[i] + h, x[:, i] + h * k3)
-        x[:, i + 1] = x[:, i] + h / 6 * (k1 + 2 * k2 + 2 * k3 + k4)
+        k1 = f(t[i], x[i])
+        k2 = f(t[i] + 0.5 * h, x[i] + 0.5 * h * k1)
+        k3 = f(t[i] + 0.5 * h, x[i] + 0.5 * h * k2)
+        k4 = f(t[i] + h, x[i] + h * k3)
+        x[i + 1] = x[i] + h / 6 * (k1 + 2 * k2 + 2 * k3 + k4)
 
     info["n_feval"] = 4 * steps
     return t, x, info
@@ -324,13 +318,13 @@ def SSPRK3(
     )
 
     t = np.linspace(t0, steps * h, steps + 1, dtype=x0.dtype)
-    x = np.zeros((x0.shape[0], steps + 1), dtype=x0.dtype)
+    x = np.zeros((steps + 1, x0.shape[0]), dtype=x0.dtype)
     t[0] = t0
-    x[:, 0] = x0
+    x[0] = x0
     for i in range(steps):
-        x1 = x[:, i] + h * f(t[i], x[:, i])
-        x2 = 3.0 / 4.0 * x[:, i] + 1.0 / 4.0 * (x1 + h * f(t[i] + h, x1))
-        x[:, i + 1] = 1.0 / 3.0 * x[:, i] + 2.0 / 3.0 * (x2 + h * f(t[i] + h / 2.0, x2))
+        x1 = x[i] + h * f(t[i], x[i])
+        x2 = 3.0 / 4.0 * x[i] + 1.0 / 4.0 * (x1 + h * f(t[i] + h, x1))
+        x[i + 1] = 1.0 / 3.0 * x[i] + 2.0 / 3.0 * (x2 + h * f(t[i] + h / 2.0, x2))
 
     info["n_feval"] = 3 * steps
     return t, x, info
@@ -356,18 +350,14 @@ def SSPRK34(
     )
 
     t = np.linspace(t0, steps * h, steps + 1, dtype=x0.dtype)
-    x = np.zeros((x0.shape[0], steps + 1), dtype=x0.dtype)
+    x = np.zeros((steps + 1, x0.shape[0]), dtype=x0.dtype)
     t[0] = t0
-    x[:, 0] = x0
+    x[0] = x0
     for i in range(steps):
-        x1 = 1.0 / 2.0 * x[:, i] + 1.0 / 2.0 * (x[:, i] + h * f(t[i], x[:, i]))
+        x1 = 1.0 / 2.0 * x[i] + 1.0 / 2.0 * (x[i] + h * f(t[i], x[i]))
         x2 = 1.0 / 2.0 * x1 + 1.0 / 2.0 * (x1 + h * f(t[i] + h / 2.0, x1))
-        x3 = (
-            2.0 / 3.0 * x[:, i]
-            + 1.0 / 6.0 * x2
-            + 1.0 / 6.0 * (x2 + h * f(t[i] + h, x2))
-        )
-        x[:, i + 1] = 1.0 / 2.0 * x3 + 1.0 / 2.0 * (x3 + h * f(t[i] + h / 2.0, x3))
+        x3 = 2.0 / 3.0 * x[i] + 1.0 / 6.0 * x2 + 1.0 / 6.0 * (x2 + h * f(t[i] + h, x2))
+        x[i + 1] = 1.0 / 2.0 * x3 + 1.0 / 2.0 * (x3 + h * f(t[i] + h / 2.0, x3))
 
     info["n_feval"] = 4 * steps
     return t, x, info
--- src/solvers/implicit.py
+++ src/solvers/implicit.py
@@ -1,6 +1,6 @@
-from typing import Callable
+from typing import Callable, Any
 import numpy as np
-from numpy.typing import NDArray, Any
+from numpy.typing import NDArray
 from scipy.optimize import root
 
 
@@ -24,27 +24,27 @@ def Backwards_Euler(
         n_restarts=0,
     )
 
-  
     t = np.linspace(t0, steps * h, steps + 1, dtype=x0.dtype)
-    x = np.zeros((x0.shape[0], steps + 1), dtype=x0.dtype)
+    x = np.zeros((steps + 1, x0.shape[0]), dtype=x0.dtype)
     t[0] = t0
-    x[:, 0] = x0
+    x[0] = x0
 
     for i in range(steps):
         t[i + 1] = t[i] + h
-        f_imp = lambda x_next: x_next - x[:, i] - h * f(t[i + 1], x_next)
-        # x[:, i + 1 : i + 2] = solver(
-        #     f_imp, x[:, i : i + 1], np.eye(x0.shape[1]), tol=solvertol
+        f_imp = lambda x_next: x_next - x[i] - h * f(t[i + 1], x_next)
+        # x[ i + 1 : i + 2] = solver(
+        #     f_imp, x[ i : i + 1], np.eye(x0.shape[1]), tol=solvertol
         # )
-        sol = root(f_imp, x0=x[:, i], tol=solvertol, method="hybr")
-        x[:, i + 1 ] = sol.x
+        sol = root(f_imp, x0=x[i], tol=solvertol, method="hybr")
+        x[i + 1] = sol.x
         if not sol.success:
             print("solver did not converge")
             break
         info["n_feval"] += sol.nfev
-        info['n_jaceval'] += sol.njev
-        info['n_lu'] += sol.nfev # TODO: is this correct?
-        print(sol.keys()); exit() # TODO: wip
+        info["n_jaceval"] += sol.njev
+        info["n_lu"] += sol.nfev  # TODO: is this correct?
+        print(sol.keys())
+        exit()  # TODO: wip
 
     return t, x, info
 
@@ -69,28 +69,29 @@ def BDF2(
         n_restarts=0,
     )
 
-
     t = np.linspace(t0, steps * h, steps + 1, dtype=x0.dtype)
-    x = np.zeros((x0.shape[0], steps + 1), dtype=x0.dtype)
-    t[:2], x[:, :2], inf_starter = Backwards_Euler(f, x0, t0 + h, h, t0, solver, solvertol)
+    x = np.zeros((steps + 1, x0.shape[0]), dtype=x0.dtype)
+    t[:2], x[:2], inf_starter = Backwards_Euler(f, x0, t0 + h, h, t0, solver, solvertol)
     info = inf_starter
 
     for i in range(steps - 1):
-        t[i + 1] = t[ i ] + h
+        t[i + 1] = t[i] + h
         f_imp = (
             lambda x_next: x_next
-            - 4 / 3 * x[:, i]
-            + 1 / 3 * x[:, i - 1]
-            - 2 / 3 * h * f(t[ i + 1], x_next)
+            - 4 / 3 * x[i]
+            + 1 / 3 * x[i - 1]
+            - 2 / 3 * h * f(t[i + 1], x_next)
         )
-        x[:, i + 1 ] = sol.x
+        sol = root(f_imp, x0=x[i], tol=solvertol, method="hybr")
+        x[i + 1] = sol.x
         if not sol.success:
             print("solver did not converge")
             break
         info["n_feval"] += sol.nfev
-        info['n_jaceval'] += sol.njev
-        info['n_lu'] += sol.nfev # TODO: is this correct?
-        print(sol.keys()); exit() # TODO: wip
+        info["n_jaceval"] += sol.njev
+        info["n_lu"] += sol.nfev  # TODO: is this correct?
+        print(sol.keys())
+        exit()  # TODO: wip
     return t, x, info
 
 
@@ -115,25 +116,26 @@ def BDF3(
     )
 
     t = np.linspace(t0, steps * h, steps + 1, dtype=x0.dtype)
-    x = np.zeros((x0.shape[0], steps + 1), dtype=x0.dtype)
-    t[:3], x[:, :3], inf_starter = BDF2(f, x0, t0 + 2 * h, h, t0, solvertol)
+    x = np.zeros((steps + 1, x0.shape[0]), dtype=x0.dtype)
+    t[:3], x[:3], inf_starter = BDF2(f, x0, t0 + 2 * h, h, t0, solvertol)
     info = inf_starter
 
     for i in range(steps - 1):
-        t[i + 1 ] = t[i ] + h
+        t[i + 1] = t[i] + h
         f_imp = (
             lambda x_next: x_next
-            - 18 / 11 * x[:, i]
-            + 9 / 11 * x[:, i - 1]
-            - 2 / 11 * x[:, i]
+            - 18 / 11 * x[i]
+            + 9 / 11 * x[i - 1]
+            - 2 / 11 * x[i]
             - 6 / 11 * h * f(t[i + 1], x_next)
         )
-        x[:, i + 1] = sol.x
+        x[i + 1] = sol.x
         if not sol.success:
             print("solver did not converge")
             break
         info["n_feval"] += sol.nfev
-        info['n_jaceval'] += sol.njev
-        info['n_lu'] += sol.nfev # TODO: is this correct?
-        print(sol.keys()); exit() # TODO: wip
+        info["n_jaceval"] += sol.njev
+        info["n_lu"] += sol.nfev  # TODO: is this correct?
+        print(sol.keys())
+        exit()  # TODO: wip
     return t, x, info
