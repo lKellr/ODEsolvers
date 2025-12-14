0067e927cbf8bd12cf480c9fce7095f5c336218d
Leo <l.kellerer1@gmx.de>
2025-12-14 17:20:28 +0100

    improvements to the case

--- problems/2body.py
+++ problems/2body.py
@@ -1,3 +1,4 @@
+from numba import njit
 import numpy as np
 from matplotlib import pyplot as plt
 import matplotlib.animation as animation
@@ -9,6 +10,7 @@ logging.basicConfig(level=logging.DEBUG)
 logger_mpb = logging.getLogger("matplotlib")
 logger_mpb.setLevel(logging.INFO)
 
+# We solve the equations nondimensionalized, with reference length 1 AU, reference time 1 year and reference mass equal to earth's mass; the second object is the moon
 masses = np.array([1.0, 0.012])
 masses2 = np.repeat(masses, 2)
 G = 4 * np.pi**2
@@ -16,6 +18,7 @@ n = masses.size
 dim = 2
 
 
+@njit
 def x_dot(t, x):
     velocities = x[n * dim :] / masses2
 
@@ -29,7 +32,7 @@ def x_dot(t, x):
             forces[slice_i] += q
             forces[slice_j] -= q
     forces *= G
-    return np.hstack([velocities, forces])
+    return np.hstack((velocities, forces))
 
 
 t_max = 1e-5
@@ -39,18 +42,12 @@ initial_positions = np.array(
         [0.002569, 0.0],
     ]
 )
-# initial_speeds = np.array(
-#     [
-#         [0.0, 0.0],
-#         [0.0, 1.59],
-#     ]
-# )
-initial_speeds = np.array(  # calculation from circular orbit
+initial_velocities = np.array(  # calculation from circular orbit
     [[0.0, 0.0]]
     + [[0.0, np.sqrt(G * masses[0] / initial_positions[i, 0])] for i in range(1, n)]
 )
 
-x0 = np.hstack([initial_positions.flatten(), masses2 * initial_speeds.flatten()])
+x0 = np.hstack((initial_positions.flatten(), masses2 * initial_velocities.flatten()))
 
 assert x0.size == (2 * n * dim), f"Wrong shape {x0.shape} of initial condition"
 
@@ -64,7 +61,7 @@ fig, ax = plt.subplots()
 for i in range(0, n * dim, dim):
     ax.plot(result[:, i], result[:, i + 1], "--", label=f"object {i//dim}")
 
-time_text = ax.text(0.5, 0.9, f"t = 0 yr", transform=ax.transAxes)
+time_text = ax.text(0.5, 0.9, f"t = 0.", transform=ax.transAxes)
 
 pcol = ax.scatter(
     result[0, : n * dim : 2], result[0, 1 : n * dim : 2], s=500 * np.log10(1 + masses)
--- problems/N_body.py
+++ problems/N_body.py
@@ -4,18 +4,23 @@ import matplotlib.animation as animation
 from solvers.embedded import DP45
 from solvers.explicit import *
 import logging
+from numba import njit
+from time import perf_counter
 
 logging.basicConfig(level=logging.DEBUG)
 logger_mpb = logging.getLogger("matplotlib")
 logger_mpb.setLevel(logging.INFO)
 
-masses = np.array([330e3, 0.0553, 0.815, 1.0, 0.107, 317.8, 95.2])
+# We solve the equations nondimensionalized, with reference length 1 AU, reference time 1 year and reference mass equal to the total sun system's mass
+masses = np.array([0.987, 1.67e-7, 2.47e-6, 3.03e-6, 3.24e-7, 9.62e-4, 2.88e-4])
 masses2 = np.repeat(masses, 2)
+names = ["Sun", "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn"]
 G = 4 * np.pi**2
 n = masses.size
 dim = 2
 
 
+@njit
 def x_dot(t, x):
     velocities = x[n * dim :] / masses2
 
@@ -29,10 +34,11 @@ def x_dot(t, x):
             forces[slice_i] += q
             forces[slice_j] -= q
     forces *= G
-    return np.hstack([velocities, forces])
+    return np.hstack((velocities, forces))
 
 
-t_max = 1e-1
+t_max = 1.0
+# TODO: use astropy ephemerides, currently i am starting with all planets in phase (on the x-axis)
 initial_positions = np.array(
     [
         [0.0, 0.0],
@@ -44,59 +50,71 @@ initial_positions = np.array(
         [9.57, 0.0],
     ]
 )
-initial_speeds = np.array(  # calculation from circular orbit
+initial_velocities = np.array(  # calculation from circular orbit
     [[0.0, 0.0]]
-    + [[0.0, np.sqrt(G * masses[0] / initial_positions[i, 0])] for i in range(1, n)]
+    + [
+        [
+            0.0,
+            np.sqrt(
+                G
+                * masses[0]
+                / np.sqrt(initial_positions[i, 0] ** 2 + initial_positions[i, 1] ** 2)
+            ),
+        ]
+        for i in range(1, n)
+    ]
 )
 
-# initial_speeds = np.array(
-#     [
-#         [0.0, 0.0],
-#         [0.0, 1.59],
-#         [0.0, 1.18],
-#         [0.0, 1.0],
-#         [0.0, 0.808],
-#         [0.0, 0.439],
-#         [0.0, 0.325],
-#     ]
-# )
-
-x0 = np.hstack([initial_positions.flatten(), masses2 * initial_speeds.flatten()])
+x0 = np.hstack([initial_positions.flatten(), masses2 * initial_velocities.flatten()])
 
 assert x0.size == (2 * n * dim), f"Wrong shape {x0.shape} of initial condition"
 
+prof_tim_start = perf_counter()
 time, result, solve_info = DP45(
     x_dot, x0, t_max, h_limits=(1e-16, np.inf), atol=1e-5, rtol=1e-3
 )
+prof_elapsed = perf_counter() - prof_tim_start
+print(f"solution took {prof_elapsed:.3f} s")  # 5.4s with numba njit, 20.2 s without
 
 
 fig, ax = plt.subplots()
 
-for i in range(0, n * dim, dim):
-    ax.plot(result[:, i], result[:, i + 1], "--", label=f"object {i//dim}")
+time_text = ax.text(
+    0.95, 0.05, f"t = 0 yr", transform=ax.transAxes, horizontalalignment="right"
+)
 
-time_text = ax.text(0.5, 0.9, f"t = 0 yr", transform=ax.transAxes)
+traces = []
+for i in range(0, n * dim, dim):
+    ax.plot(result[:, i], result[:, i + 1], "--", alpha=0.5)
+    traces.append(ax.plot(result[:1, i], result[:1, i + 1], label=names[i // dim])[0])
 
 pcol = ax.scatter(
-    result[0, : n * dim : 2], result[0, 1 : n * dim : 2], s=500 * np.log10(1 + masses)
+    result[0, : n * dim : 2],
+    result[0, 1 : n * dim : 2],
+    s=5e3 * np.log10(1 + masses),
 )
 
 
 def update(frame):
-    time_text.set_text(f"t = {time[frame]}")
-    # pcol.set_sizes(500 * np.log10(1 + masses))
+    time_text.set_text(f"t = {time[frame]:.2f} yr")
+    for i in range(n):
+        traces[i].set_data(
+            result[: frame + 1, i * dim], result[: frame + 1, i * dim + 1]
+        )
     pcol.set_offsets(
         result[frame, : n * dim].reshape(-1, dim)
     )  # TODO: willl not work for 3D
     return (
         time_text,
+        traces,
         pcol,
     )
 
 
 ani = animation.FuncAnimation(
-    fig=fig, func=update, frames=time.size, interval=500, repeat=True
+    fig=fig, func=update, frames=time.size, interval=200, repeat=True
 )
 ax.set_aspect("equal", "box")
 ax.legend()
+ani.save("N-body.mp4", fps=5)
 plt.show()
