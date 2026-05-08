9a47841ee3c8ca5a97bc38da2afeb3f629b66da6
l.kellerer <l.kellerer1@gmx.de>
2026-05-09 00:32:02 +0200

    convergence measurement with minimum h

--- problems/convergence.py
+++ problems/convergence.py
@@ -22,68 +22,94 @@ logger_pil.setLevel(logging.INFO)
 cmap = plt.get_cmap("tab20")
 
 
-x_dot: Callable[[float, NDArray[np.floating]], NDArray[np.floating]] = lambda t, x: x*(2. - np.sin(t))
+x_dot: Callable[[float, NDArray[np.floating]], NDArray[np.floating]] = (
+    lambda t, x: x * (2.0 - np.sin(t))
+)
 jac: Callable[[float, NDArray[np.floating]], NDArray[np.floating]] = (
     lambda t, x: 2.0 - np.sin(t)
 )
 
 t_max = 1.0
-x0 = np.array([2.])
+x0 = np.array([2.0])
 
-x_analytic: Callable[[float], NDArray[np.floating]] = lambda t: 2*np.exp(2*t + np.cos(t) - 1.)
+x_analytic: Callable[[float], NDArray[np.floating]] = lambda t: 2 * np.exp(
+    2 * t + np.cos(t) - 1.0
+)
 
 norm = norm_hairer
 
-N_list = 4*np.array([2 ** (k // 2) if k == 1 or k % 2 == 0 else 1.5 * 2 ** (k // 2) for k in range(1,8)])
+N_list = 4 * np.array(
+    [
+        2 ** (k // 2) if k == 1 or k % 2 == 0 else 1.5 * 2 ** (k // 2)
+        for k in range(1, 10)
+    ]
+)
 k_list = np.array(range(5))
 conv_data = dict()
 
 errors = list()
+h_mins = list()
 for n_steps in N_list:
-  time, result, solve_info = AB3(x_dot, x0, t_max, h=t_max/n_steps)
-  errors.append(norm(result[-1] - x_analytic(time[-1])))
-conv_data['AB_3'] = np.array(errors)
+    time, result, solve_info = AB3(x_dot, x0, t_max, h=t_max / n_steps)
+    errors.append(norm(result[-1] - x_analytic(time[-1])))
+    h_mins.append(t_max / n_steps)
+conv_data["AB_3"] = np.array(errors), np.array(h_mins)
 
 errors = list()
+h_mins = list()
 for n_steps in N_list:
-  time, result, solve_info = RK4(x_dot, x0, t_max, h=t_max/n_steps)
-  errors.append(norm(result[-1] - x_analytic(time[-1])))
-conv_data['RK4'] = np.array(errors)
+    time, result, solve_info = RK4(x_dot, x0, t_max, h=t_max / n_steps)
+    errors.append(norm(result[-1] - x_analytic(time[-1])))
+    h_mins.append(t_max / n_steps)
+conv_data["RK4"] = np.array(errors), np.array(h_mins)
 
 errors = list()
+h_mins = list()
 for n_steps in N_list:
-  solver_eulex2 = EulerExtrapolation(
-    ode_fun=x_dot,
-    table_size=8,
-    step_controller=StepControllerExtrapDummy(),
-  )
-  time, result, solve_info = solver_eulex2.solve(x0, t_max, k_initial=1, h_initial=t_max/n_steps)
-  errors.append(norm(result[-1]-x_analytic(time[-1])))
-conv_data['EULEX2'] = np.array(errors)
+    solver_eulex2 = EulerExtrapolation(
+        ode_fun=x_dot,
+        table_size=8,
+        step_controller=StepControllerExtrapDummy(),
+    )
+    time, result, solve_info = solver_eulex2.solve(
+        x0, t_max, k_initial=1, h_initial=t_max / n_steps
+    )
+    errors.append(norm(result[-1] - x_analytic(time[-1])))
+    h_mins.append(np.mean(solve_info["h_min"]))
+conv_data["EULEX2"] = np.array(errors), np.array(h_mins)
 
 errors = list()
+h_mins = list()
 for n_steps in N_list:
-  solver_eulex3 = EulerExtrapolation(
-    ode_fun=x_dot,
-    table_size=8,
-    step_controller=StepControllerExtrapDummy(),
-  )
-  time, result, solve_info = solver_eulex3.solve(x0, t_max, k_initial=2, h_initial=t_max/n_steps)
-  errors.append(norm(result[-1]-x_analytic(time[-1])))
-conv_data['EULEX3'] = np.array(errors)
+    solver_eulex3 = EulerExtrapolation(
+        ode_fun=x_dot,
+        table_size=8,
+        step_controller=StepControllerExtrapDummy(),
+    )
+    time, result, solve_info = solver_eulex3.solve(
+        x0, t_max, k_initial=2, h_initial=t_max / n_steps
+    )
+    errors.append(norm(result[-1] - x_analytic(time[-1])))
+    h_mins.append(np.mean(solve_info["h_min"]))
+conv_data["EULEX3"] = np.array(errors), np.array(h_mins)
 
 errors = list()
+h_mins = list()
 for n_steps in N_list:
-  solver_eulex5 = EulerExtrapolation(
-    ode_fun=x_dot,
-    table_size=8,
-    step_controller=StepControllerExtrapDummy(),
-  )
-  time, result, solve_info = solver_eulex5.solve(x0, t_max, k_initial=4, h_initial=t_max/n_steps)
-  errors.append(norm(result[-1]-x_analytic(time[-1])))
-conv_data['EULEX5'] = np.array(errors)
+    solver_eulex5 = EulerExtrapolation(
+        ode_fun=x_dot,
+        table_size=8,
+        step_controller=StepControllerExtrapDummy(),
+    )
+    time, result, solve_info = solver_eulex5.solve(
+        x0, t_max, k_initial=4, h_initial=t_max / n_steps
+    )
+    errors.append(norm(result[-1] - x_analytic(time[-1])))
+    h_mins.append(np.mean(solve_info["h_min"]))
+conv_data["EULEX5"] = np.array(errors), np.array(h_mins)
 
 errors = list()
+h_mins = list()
 for n_steps in N_list:
     solver_odex_rat = ModMidpointExtrapolationRational(
         ode_fun=x_dot,
@@ -94,20 +120,27 @@ for n_steps in N_list:
         x0, t_max, k_initial=4, h_initial=t_max / n_steps
     )
     errors.append(norm(result[-1] - x_analytic(time[-1])))
-conv_data["ODEX5_rational"] = np.array(errors)
+    h_mins.append(np.mean(solve_info["h_min"]))
+conv_data["ODEX5_rational_smoothed"] = np.array(errors), np.array(h_mins)
 
 errors = list()
+h_mins = list()
 for n_steps in N_list:
-  solver_eulex7 = EulerExtrapolation(
-    ode_fun=x_dot,
-    table_size=8,
-    step_controller=StepControllerExtrapDummy(),
-  )
-  time, result, solve_info = solver_eulex7.solve(x0, t_max, k_initial=6, h_initial=t_max/n_steps)
-  errors.append(norm(result[-1]-x_analytic(time[-1])))
-conv_data['EULEX7'] = np.array(errors)
+    solver_eulex7 = EulerExtrapolation(
+        ode_fun=x_dot,
+        table_size=8,
+        step_controller=StepControllerExtrapDummy(),
+    )
+    time, result, solve_info = solver_eulex7.solve(
+        x0, t_max, k_initial=6, h_initial=t_max / n_steps
+    )
+    errors.append(norm(result[-1] - x_analytic(time[-1])))
+    h_mins.append(np.mean(solve_info["h_min"]))
+conv_data["EULEX7"] = np.array(errors), np.array(h_mins)
+
 
 errors = list()
+h_mins = list()
 for n_steps in N_list:
     solver_eulex7ld = EulerExtrapolation(
         ode_fun=x_dot,
@@ -119,10 +152,12 @@ for n_steps in N_list:
         x0.astype(np.longdouble), t_max, k_initial=6, h_initial=t_max / n_steps
     )
     errors.append(norm(result[-1] - x_analytic(time[-1])))
-conv_data["EULEX7_longdouble"] = np.array(errors)
+    h_mins.append(np.mean(solve_info["h_min"]))
+conv_data["EULEX7_longdouble"] = np.array(errors), np.array(h_mins)
 
 # NOTE: ODEX schemes converge at higher order than predicted for linear problems
 errors = list()
+h_mins = list()
 for n_steps in N_list:
     solver_odex3 = ModMidpointExtrapolation(
         ode_fun=x_dot, table_size=8, step_controller=StepControllerExtrapDummy()
@@ -131,9 +166,11 @@ for n_steps in N_list:
         x0, t_max, k_initial=1, h_initial=t_max / n_steps
     )
     errors.append(norm(result[-1] - x_analytic(time[-1])))
-conv_data["ODEX3"] = np.array(errors)
+    h_mins.append(np.mean(solve_info["h_min"]))
+conv_data["ODEX3"] = np.array(errors), np.array(h_mins)
 
 errors = list()
+h_mins = list()
 for n_steps in N_list:
     solver_odex3s = ModMidpointExtrapolation(
         ode_fun=x_dot,
@@ -145,9 +182,11 @@ for n_steps in N_list:
         x0, t_max, k_initial=1, h_initial=t_max / n_steps
     )
     errors.append(norm(result[-1] - x_analytic(time[-1])))
-conv_data["ODEX3_smoothed"] = np.array(errors)
+    h_mins.append(np.mean(solve_info["h_min"]))
+conv_data["ODEX3_smoothed"] = np.array(errors), np.array(h_mins)
 
 errors = list()
+h_mins = list()
 for n_steps in N_list:
     solver_odex5 = ModMidpointExtrapolation(
         ode_fun=x_dot, table_size=8, step_controller=StepControllerExtrapDummy()
@@ -156,7 +195,37 @@ for n_steps in N_list:
         x0, t_max, k_initial=2, h_initial=t_max / n_steps
     )
     errors.append(norm(result[-1] - x_analytic(time[-1])))
-conv_data["ODEX5"] = np.array(errors)
+    h_mins.append(np.mean(solve_info["h_min"]))
+conv_data["ODEX5"] = np.array(errors), np.array(h_mins)
+
+errors = list()
+h_mins = list()
+for n_steps in N_list:
+    solver_odex7 = ModMidpointExtrapolation(
+        ode_fun=x_dot, table_size=8, step_controller=StepControllerExtrapDummy()
+    )
+    time, result, solve_info = solver_odex7.solve(
+        x0, t_max, k_initial=3, h_initial=t_max / n_steps
+    )
+    errors.append(norm(result[-1] - x_analytic(time[-1])))
+    h_mins.append(np.mean(solve_info["h_min"]))
+conv_data["ODEX7"] = np.array(errors), np.array(h_mins)
+
+errors = list()
+h_mins = list()
+for n_steps in N_list:
+    solver_odex7ld = ModMidpointExtrapolation(
+        ode_fun=x_dot,
+        table_size=8,
+        step_controller=StepControllerExtrapDummy(),
+        dtype=np.longdouble,
+    )
+    time, result, solve_info = solver_odex7ld.solve(
+        x0.astype(np.longdouble), t_max, k_initial=3, h_initial=t_max / n_steps
+    )
+    errors.append(norm(result[-1] - x_analytic(time[-1])))
+    h_mins.append(np.mean(solve_info["h_min"]))
+conv_data["ODEX7_longdouble"] = np.array(errors), np.array(h_mins)
 
 errors = list()
 for n_steps in N_list:
@@ -171,9 +240,11 @@ for n_steps in N_list:
         x0, t_max, k_initial=1, h_initial=t_max / n_steps
     )
     errors.append(norm(result[-1] - x_analytic(time[-1])))
-conv_data["SEULEX2"] = np.array(errors)
+    h_mins.append(np.mean(solve_info["h_min"]))
+conv_data["SEULEX2"] = np.array(errors), np.array(h_mins)
 
 errors = list()
+h_mins = list()
 for n_steps in N_list:
     solver_seulex2 = LimplicitEulerExtrapolation(
         ode_fun=x_dot,
@@ -186,9 +257,11 @@ for n_steps in N_list:
         x0, t_max, k_initial=4, h_initial=t_max / n_steps
     )
     errors.append(norm(result[-1] - x_analytic(time[-1])))
-conv_data["SEULEX5"] = np.array(errors)
+    h_mins.append(np.mean(solve_info["h_min"]))
+conv_data["SEULEX5"] = np.array(errors), np.array(h_mins)
 
 errors = list()
+h_mins = list()
 for n_steps in N_list:
     solver_seulex2_nj = LimplicitEulerExtrapolation(
         ode_fun=x_dot,
@@ -201,9 +274,11 @@ for n_steps in N_list:
         x0, t_max, k_initial=4, h_initial=t_max / n_steps
     )
     errors.append(norm(result[-1] - x_analytic(time[-1])))
-conv_data["SEULEX5_numjac"] = np.array(errors)
+    h_mins.append(np.mean(solve_info["h_min"]))
+conv_data["SEULEX5_numjac"] = np.array(errors), np.array(h_mins)
 
 errors = list()
+h_mins = list()
 for n_steps in N_list:
     solver_sodex3 = LimplicitMidpointExtrapolation(
         ode_fun=x_dot,
@@ -217,9 +292,11 @@ for n_steps in N_list:
         x0, t_max, k_initial=1, h_initial=t_max / n_steps
     )
     errors.append(norm(result[-1] - x_analytic(time[-1])))
-conv_data["SODEX3"] = np.array(errors)
+    h_mins.append(np.mean(solve_info["h_min"]))
+conv_data["SODEX3"] = np.array(errors), np.array(h_mins)
 
 errors = list()
+h_mins = list()
 for n_steps in N_list:
     solver_sodex3s = LimplicitMidpointExtrapolation(
         ode_fun=x_dot,
@@ -233,19 +310,20 @@ for n_steps in N_list:
         x0, t_max, k_initial=1, h_initial=t_max / n_steps
     )
     errors.append(norm(result[-1] - x_analytic(time[-1])))
-conv_data["SODEX3smoothed"] = np.array(errors)
+    h_mins.append(np.mean(solve_info["h_min"]))
+conv_data["SODEX3_smoothed"] = np.array(errors), np.array(h_mins)
 
 # results
 fig, ax = plt.subplots(figsize=(9.6, 7.2))
 # ax.set_ylim(-5, 5)
 ax.set_xscale("log")
 ax.set_yscale("log")
-ax.set_xlabel("h")
+ax.set_xlabel("H")
 ax.set_ylabel("error")
 
-for i, (scheme_name, errors) in enumerate(conv_data.items()):
-    ax.plot(t_max/N_list, errors, label=scheme_name, color=cmap(i), marker='o')
-    rate = np.log(errors[1:]/errors[:-1])/np.log(N_list[:-1]/N_list[1:])
+for i, (scheme_name, (errors, _)) in enumerate(conv_data.items()):
+    ax.plot(t_max / N_list, errors, label=scheme_name, color=cmap(i), marker="o")
+    rate = np.log(errors[1:] / errors[:-1]) / np.log(N_list[:-1] / N_list[1:])
     ax.text(t_max / N_list[-1], errors[-1], rf"$\bar{{p}} = {np.mean(rate):.2f}$")
     ax.text(t_max / N_list[0], errors[0], f"$p_0 = {rate[0]:.2f}$")
 
@@ -253,4 +331,23 @@ for i, (scheme_name, errors) in enumerate(conv_data.items()):
 plt.legend(frameon=False)
 plt.tight_layout()
 # plt.savefig("convergence.png")
+plt.show()
+
+# plot over minimum h
+fig, ax = plt.subplots(figsize=(9.6, 7.2))
+# ax.set_ylim(-5, 5)
+ax.set_xscale("log")
+ax.set_yscale("log")
+ax.set_xlabel(r"$h_\mathrm{min}$")
+ax.set_ylabel("error")
+
+for i, (scheme_name, (errors, h_min)) in enumerate(conv_data.items()):
+    ax.plot(h_min, errors, label=scheme_name, color=cmap(i), marker="o")
+    rate = np.log(errors[1:] / errors[:-1]) / np.log(h_min[1:] / h_min[:-1])
+    ax.text(h_min[-1], errors[-1], rf"$\bar{{p}} = {np.mean(rate):.2f}$")
+    ax.text(h_min[0], errors[0], f"$p_0 = {rate[0]:.2f}$")
+
+
+plt.legend(frameon=False)
+plt.tight_layout()
 plt.show()
